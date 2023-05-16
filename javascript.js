518181cc7d2bb29e1c34c720c2730ff879526ff1
Duh0127 <rodatupmocgamer22@gmail.com>
2023-05-16 17:11:53 -0300

    Selecionando elemento CONFSENHA com GetElementById

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@
 //Verificação de confirmação de Senha
 var botaoEntrar = document.getElementById("botao");
 var inputSenha = document.getElementById("senha");
+var inputConfSenha = document.getElementById("confirmaSenha");
 
 
 
