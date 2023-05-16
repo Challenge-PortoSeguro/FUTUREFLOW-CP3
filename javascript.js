36572d22a2ef9f61ed56e43f6d2fc9173594cad7
Duh0127 <rodatupmocgamer22@gmail.com>
2023-05-16 17:13:09 -0300

    Condição criada, utilizando alert()

--- javascript.js
+++ javascript.js
@@ -7,5 +7,10 @@ var inputSenha = document.getElementById("senha");
 var inputConfSenha = document.getElementById("confirmaSenha");
 
 botaoEntrar.addEventListener("click", ()=>{
-    
+    if(inputSenha.value == inputConfSenha.value){
+        alert("Senha Validada!!!");
+    }
+    else{
+        alert("Senha Não Compatível!!!");
+    }
 });
