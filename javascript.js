054f0272a5785d0bbe3989e70312ee6eae33bec7
Duh0127 <rodatupmocgamer22@gmail.com>
2023-05-17 10:15:05 -0300

    Alteração na validação de senha nula

--- javascript.js
+++ javascript.js
@@ -3,14 +3,12 @@ function verifSenha(){
     var inputSenha = document.getElementById("senha");
     var inputConfSenha = document.getElementById("confirmaSenha");
 
-    if(inputSenha.value == "" && inputConfSenha.value == ""){
-        alert("Digite uma senha!");
-    }
-    else if(inputSenha.value == inputConfSenha.value){
-        alert("Senha Verificada!")
+    if(inputSenha.value != "" && inputConfSenha.value != ""){
+        
     }
     else{
-        alert("Senha Não Compatível!");
+        alert("Senha Deve ser Preenchida!");
     }
 }
+
 
