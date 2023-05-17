4c8a313ddca9a362fae02a521ce8a8d747b17016
Duh0127 <rodatupmocgamer22@gmail.com>
2023-05-17 10:15:43 -0300

    Alteração na validação de senha > 5 caracteres

--- javascript.js
+++ javascript.js
@@ -4,7 +4,12 @@ function verifSenha(){
     var inputConfSenha = document.getElementById("confirmaSenha");
 
     if(inputSenha.value != "" && inputConfSenha.value != ""){
-        
+        if(inputSenha.value.length > 5 && inputConfSenha.value.length > 5){
+            
+        }
+        else{
+            alert("Senha com menos de 6 caracteres!");
+        }
     }
     else{
         alert("Senha Deve ser Preenchida!");
